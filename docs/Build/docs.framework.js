1bbe1d2e961db956c7bfecb14dad360dfa4bd793
dokudami <69578376+ysdzm@users.noreply.github.com>
2023-04-20 00:46:37 +0900

    BlobIOテストシーンのビルド

--- docs/Build/docs.framework.js
+++ docs/Build/docs.framework.js
@@ -1159,26 +1159,26 @@ var tempDouble;
 var tempI64;
 
 var ASM_CONSTS = {
- 3405556: function() {
+ 3405284: function() {
   Module["emscripten_get_now_backup"] = performance.now;
  },
- 3405611: function($0) {
+ 3405339: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3405659: function($0) {
+ 3405387: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3405707: function() {
+ 3405435: function() {
   performance.now = Module["emscripten_get_now_backup"];
  },
- 3405762: function() {
+ 3405490: function() {
   return Module.webglContextAttributes.premultipliedAlpha;
  },
- 3405823: function() {
+ 3405551: function() {
   return Module.webglContextAttributes.preserveDrawingBuffer;
  }
 };
@@ -1280,6 +1280,80 @@ function stackTrace() {
  var js = jsStackTrace();
  if (Module["extraStackTrace"]) js += "\n" + Module["extraStackTrace"]();
  return demangleAll(js);
+}
+
+function _BlobIOInitialize(uploadCallbackPtr, overlayHtmlPtr) {
+ if (!document.getElementById("blobio-upload-overlay")) {
+  var element = document.createElement("div");
+  var overlayHtml = Pointer_stringify(overlayHtmlPtr);
+  if (overlayHtml) {
+   element.innerHTML = overlayHtml;
+  } else {
+   var overlayHtmlDefault = '<div id="blobio-upload-overlay" style="background-color: rgba(0, 0, 0, 0.75); position:absolute; width: 100%; height: 100%; margin: 0; top: 0; display: table; color: #fff;"><div id="blobio-upload-area" style="position: relative; width: 100%; height: 100%; cursor:pointer;"><div id="blobio-upload-container" style="position: absolute; top: 50%; left: 50%; transform: translateY(-50%) translateX(-50%); text-align: center;"><span>Click here to select file...</span><br><input type="button" value="Cancel" id="blobio-button-cancel"><input type="file" style="display: none" id="blobio-file"></div></div></div>';
+   element.innerHTML = overlayHtmlDefault;
+  }
+  document.getElementById("unityContainer").appendChild(element);
+  document.getElementById("blobio-button-cancel").addEventListener("click", function(e) {
+   document.getElementById("blobio-upload-overlay").style.display = "none";
+   e.stopPropagation();
+  });
+  var uploadArea = document.getElementById("blobio-upload-area");
+  uploadArea.addEventListener("click", function(e) {
+   document.getElementById("blobio-file").click();
+  });
+  document.getElementById("blobio-file").addEventListener("change", function(changeEvt) {
+   var files = changeEvt.target.files;
+   if (files.length == 0) return;
+   var reader = new FileReader();
+   reader.addEventListener("load", function(loadEvt) {
+    var state = uploadArea.dataset.state;
+    var buffer = new Uint8Array(loadEvt.target.result);
+    var length = buffer.byteLength;
+    console.log(length);
+    var dataPtr = _malloc(length);
+    HEAPU8.set(buffer, dataPtr);
+    var filename = files[0].name;
+    var filenameSize = lengthBytesUTF8(filename) + 1;
+    var filenamePtr = _malloc(filenameSize);
+    stringToUTF8(filename, filenamePtr, filenameSize);
+    Runtime.dynCall("viiii", uploadCallbackPtr, [ state, dataPtr, length, filenamePtr ]);
+    _free(dataPtr);
+    _free(filenamePtr);
+   });
+   reader.readAsArrayBuffer(files[0]);
+   document.getElementById("blobio-upload-overlay").style.display = "none";
+  }, false);
+  document.getElementById("blobio-upload-overlay").style.display = "none";
+ }
+}
+
+function _BlobIOMakeDownload(array, length, mime, filename) {
+ var subArray = HEAPU8.subarray(array, array + length);
+ var mimeStr = Pointer_stringify(mime);
+ var filenameStr = Pointer_stringify(filename);
+ var blob = new Blob([ subArray ], {
+  type: mimeStr
+ });
+ var url = window.URL.createObjectURL(blob);
+ console.log(url);
+ var a = window.document.createElement("a");
+ window.document.body.appendChild(a);
+ a.download = filenameStr;
+ a.href = url;
+ a.click();
+ a.remove();
+ window.URL.revokeObjectURL(url);
+}
+
+function _BlobIOMakeUpload(state, accept) {
+ var overlay = document.getElementById("blobio-upload-overlay");
+ if (!overlay) {
+  alert("BlobIO: Overlay is not found.");
+  return;
+ }
+ overlay.style.display = "initial";
+ document.getElementById("blobio-upload-area").dataset.state = state;
+ document.getElementById("blobio-file").accept = Pointer_stringify(accept);
 }
 
 var JS_Accelerometer = null;
@@ -12951,6 +13025,9 @@ function intArrayFromString(stringy, dontAddNull, length) {
 }
 
 var asmLibraryArg = {
+ "BlobIOInitialize": _BlobIOInitialize,
+ "BlobIOMakeDownload": _BlobIOMakeDownload,
+ "BlobIOMakeUpload": _BlobIOMakeUpload,
  "JS_Accelerometer_IsRunning": _JS_Accelerometer_IsRunning,
  "JS_Accelerometer_Start": _JS_Accelerometer_Start,
  "JS_Accelerometer_Stop": _JS_Accelerometer_Stop,
